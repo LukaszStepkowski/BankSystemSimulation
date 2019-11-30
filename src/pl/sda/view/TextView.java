23b5d63c87010f52852fe436f70164b9966f7778
Lukasz Stepkowski <l.stepkow@gmail.com>
2019-11-30 10:26:52 +0100

    added view layer

---
+++ src/pl/sda/view/TextView.java
@@ -0,0 +1,20 @@
+package pl.sda.view;
+
+import java.util.Scanner;
+
+public class TextView {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj login:");
+        String login = scanner.next();
+
+        System.out.println("Podaj hasło:");
+        String password = scanner.next();
+
+
+
+    }
+}
