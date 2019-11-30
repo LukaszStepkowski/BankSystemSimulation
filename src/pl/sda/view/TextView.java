8a05d8db0cc6dab3747c9a9eab63faf9133f70f7
Lukasz Stepkowski <l.stepkow@gmail.com>
2019-11-30 11:01:58 +0100

    tested authentication method to TextView

--- src/pl/sda/view/TextView.java
+++ src/pl/sda/view/TextView.java
@@ -1,5 +1,8 @@
 package pl.sda.view;
 
+import pl.sda.entity.Client;
+import pl.sda.service.LoginService;
+
 import java.util.Scanner;
 
 public class TextView {
@@ -7,6 +10,9 @@ public class TextView {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        LoginService loginService = new LoginService();
+
+        Client client = null;
 
         System.out.println("Podaj login:");
         String login = scanner.next();
@@ -14,7 +20,15 @@ public class TextView {
         System.out.println("Podaj hasło:");
         String password = scanner.next();
 
+        if (loginService.isAuthenticated(login, password)){
+            client = loginService.getClientByLoginAndPassword(login, password);
 
+            System.out.println("Logowanie udane");
+
+        } else {
+            System.out.println("Logowanie się nie powiodło");
+            System.exit(0);
+        }
 
     }
 }
