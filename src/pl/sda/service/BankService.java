75dd0c27aa4d041c51addd4c9fcc134760c467e8
Lukasz Stepkowski <l.stepkow@gmail.com>
2019-12-01 14:57:31 +0100

    added constructor and get, save methods to BankService

--- src/pl/sda/service/BankService.java
+++ src/pl/sda/service/BankService.java
@@ -1,13 +1,33 @@
 package pl.sda.service;
 
+import pl.sda.dao.ClientDAO;
+import pl.sda.dao.ClientDAOFile;
 import pl.sda.entity.Account;
+import pl.sda.entity.Client;
 import pl.sda.exception.IndexValidationException;
 import pl.sda.exception.InsufficientBalanceException;
 import pl.sda.exception.NegativeAmountException;
 
+import java.io.IOException;
 import java.util.List;
 
 public class BankService {
+
+    private List<Client> clients;
+    private ClientDAO clientDAO;
+
+    public BankService() throws IOException {
+        clientDAO = new ClientDAOFile();
+        clients = clientDAO.get();
+    }
+
+    public List<Client> get(){
+        return clients;
+    }
+
+    public void save() throws IOException {
+        clientDAO.save(clients);
+    }
 
     public void payment (List<Account> accounts, double amount, int index) throws IndexValidationException, NegativeAmountException {
 
