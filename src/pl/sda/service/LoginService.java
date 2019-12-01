ef1e7c6f276c74025f13273341e708aa191ff117
Lukasz Stepkowski <l.stepkow@gmail.com>
2019-12-01 15:04:07 +0100

    added constructor to LoginService, corrected isAuthenticated, getClientByLoginAndPassword methods

--- src/pl/sda/service/LoginService.java
+++ src/pl/sda/service/LoginService.java
@@ -1,25 +1,18 @@
 package pl.sda.service;
 
-import pl.sda.dao.ClientDAO;
-import pl.sda.dao.ClientDAOFile;
 import pl.sda.entity.Client;
 
-import java.io.IOException;
 import java.util.List;
 
 public class LoginService {
 
-    private ClientDAO clientDAO = new ClientDAOFile();
+    private List<Client> clients;
+
+    public LoginService(List<Client> clients){
+        this.clients = clients;
+    }
 
     public boolean isAuthenticated(String login, String password) {
-
-        List<Client> clients;
-
-        try {
-            clients = clientDAO.get();
-        } catch (IOException e) {
-            return false;
-        }
 
         for(Client client : clients){
 
@@ -33,14 +26,6 @@ public class LoginService {
     }
 
     public Client getClientByLoginAndPassword (String login, String password){
-
-        List<Client> clients;
-
-        try {
-            clients = clientDAO.get();
-        } catch (IOException e) {
-            return null;
-        }
 
         return clients.stream()
                 .filter(c -> c.getLogin().equals(login) && c.getPassword().equals(password))
